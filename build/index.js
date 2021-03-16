203fc00eb4b4b1b29284a44ab1b6b2c2543af25a
dachingygang@gmail.com <dachingygang@gmail.com>
2021-03-16 16:33:17 +1300

    Workingdir is cleared on startup

--- build/index.js
+++ build/index.js
@@ -6,7 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const overcord_1 = require("@frasermcc/overcord");
 const path_1 = __importDefault(require("path"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const fs_1 = __importDefault(require("fs"));
 dotenv_1.default.config();
+(async () => {
+    let workingDirPath = "./workingdir";
+    // Clear working dir on startup.
+    fs_1.default.rmdirSync(workingDirPath, { recursive: true });
+    fs_1.default.mkdirSync(workingDirPath);
+})();
 (async () => {
     const client = new overcord_1.Client({ defaultCommandPrefix: "a.", owners: [], disableMentions: "everyone" });
     await client.registry.recursivelyRegisterCommands(path_1.default.join(__dirname, "/commands"));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Client } from "@frasermcc/overcord";
 import path from "path";
 import dotenv from "dotenv";
+import fs from 'fs';
 dotenv.config();
+(async () => {
+    let workingDirPath = "./workingdir";
+    // Clear working dir on startup.
+    fs.rmdirSync(workingDirPath, {recursive: true});
+    fs.mkdirSync(workingDirPath);
+})();
 (async () => {
     const client = new Client({ defaultCommandPrefix: "a.", owners: [], disableMentions: "everyone" });
     await client.registry.recursivelyRegisterCommands(path.join(__dirname, "/commands"));
