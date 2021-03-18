c75e617582cca6c4c657fa360e6db33d6853c11f
dachingygang@gmail.com <dachingygang@gmail.com>
2021-03-18 14:31:58 +1300

    dlss cleanup

--- src/commands/dlss.ts
+++ src/commands/dlss.ts
@@ -25,11 +25,9 @@ export default class TestCommand extends Command {
         let attachments = message.attachments;
         if(attachments.size == 0) {
             message.channel.send("You must attach a single image to use this function (set the caption to a.dlss <scale>)");
-            console.log("User failed to attach image (no attachment found)\n");
             return;
         } else if (attachments.size > 1) {
             message.channel.send("This function does not upscale multiple images at once.");
-            console.log("User attached multiple images (not supported)\n");
             return;
         }
         let image = attachments.values().next().value;
@@ -37,7 +35,6 @@ export default class TestCommand extends Command {
 
         if(!(image.width > 0)) {
             message.channel.send("Your attachment does not appear to be an image.");
-            console.log("User failed to attach image (attachment is not an image)\n");
             return;
         } 
 
@@ -49,44 +46,40 @@ export default class TestCommand extends Command {
         }
         
         message.channel.send("Downloading image...");
-        console.log("Downloading image...");
 
         let date: string = Date.now().toString();
 
         let fileName: string = "./workingdir/dlss_image_" + date;
 
-        request.get(image.url)
+        await request.get(image.url)
             .on('error', console.error)
-            .pipe(fs.createWriteStream(fileName + "." + extension).on("close", async function(){
-                await message.channel.send("Image recieved. Scaling to " + scalefactor + "x resolution (" + (image.width * scalefactor).toString() + "x"
-                                + (image.height * scalefactor).toString()  + ")");
-                console.log("Image recieved. Scaling to " + scalefactor + "x resolution (" + (image.width * scalefactor).toString() + "x"
-                + (image.height * scalefactor).toString()  + ")");
-                try {
-                    await Waifu2x.upscaleImage(fileName + "." + extension, fileName + "-out." + extension, {
-                        scale: scalefactor
-                    });
-                } catch(err) {
-                    message.channel.send(err.toString());
-                    return;
-                }
-                let fileSize: number = fs.statSync(fileName + "-out." + extension).size / 1000000.0;
-                if (fileSize >= 100) {
-                    message.channel.send("Output image is greater than 100 MB. Image will not send.");
-                    return;
-                } else {
-                    message.channel.send("Upscaling completed, attempting to upload "+ fileSize.toFixed(2) + "MB...");
-                }
-                let outputImage = fs.readFileSync(fileName + "-out." + extension); 
-                message.channel.send("", {files: [fileName + "-out." + extension]}).catch(error => {
-                    console.log("Output image was too large.");
-                    if(error['code'] === 40005) {
-                        message.channel.send("Image was too large to output.");
-                    }
-                }).then(() => {
-                    fs.unlinkSync(fileName + "-out." + extension);
-                    fs.unlinkSync(fileName + "." + extension);
-                });
-            }));
+            .pipe(fs.createWriteStream(fileName + "." + extension));
+
+        await message.channel.send("Image recieved. Scaling to " + scalefactor + "x resolution (" + (image.width * scalefactor).toString() + "x"
+                        + (image.height * scalefactor).toString()  + ")");
+        try {
+            await Waifu2x.upscaleImage(fileName + "." + extension, fileName + "-out." + extension, {
+                scale: scalefactor
+            });
+        } catch(err) {
+            message.channel.send(err.toString());
+            return;
+        }
+        let fileSize: number = fs.statSync(fileName + "-out." + extension).size / 1000000.0;
+        if (fileSize >= 100) {
+            message.channel.send("Output image is greater than 100 MB. Image will not send.");
+            return;
+        } else {
+            message.channel.send("Upscaling completed, attempting to upload "+ fileSize.toFixed(2) + "MB...");
+        }
+        let outputImage = fs.readFileSync(fileName + "-out." + extension); 
+
+        await message.channel.send("", {files: [fileName + "-out." + extension]}).catch(error => {
+            if(error['code'] === 40005) {
+                message.channel.send("Image was too large to output.");
+            }
+        });
+        fs.unlinkSync(fileName + "-out." + extension);
+        fs.unlinkSync(fileName + "." + extension);
     }
 }
